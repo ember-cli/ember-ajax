9cf8774dd082fea5876a9adee9136159f9306ffb
Vikram Ramakrishnan <lord.ezar@gmail.com>
2016-02-12 19:46:09 -0500

    refactor parse-response-headers & add tests

--- addon/utils/parse-response-headers.js
+++ addon/utils/parse-response-headers.js
@@ -1,21 +1,24 @@
-export default function parseResponseHeaders(headerStr) {
-  const headers = Object.create(null);
-  if (!headerStr) {
+const CLRF = '\u000d\u000a';
+
+export default function parseResponseHeaders(headersString) {
+  let headers = {};
+
+  if (!headersString) {
     return headers;
   }
 
-  const headerPairs = headerStr.split('\u000d\u000a');
-  for (let i = 0; i < headerPairs.length; i++) {
-    const headerPair = headerPairs[i];
-    // Can't use split() here because it does the wrong thing
-    // if the header value has the string ": " in it.
-    const index = headerPair.indexOf('\u003a\u0020');
-    if (index > 0) {
-      const key = headerPair.substring(0, index);
-      const val = headerPair.substring(index + 2);
-      headers[key] = val;
+  let headerPairs = headersString.split(CLRF);
+
+  headerPairs.forEach((header) => {
+    let [field, ...value] = header.split(':');
+
+    field = field.trim();
+    value = value.join(':').trim();
+
+    if (value) {
+      headers[field] = value;
     }
-  }
+  });
 
   return headers;
 }
