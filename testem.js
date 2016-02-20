69f7fc4d8bcbb1ced3f8fd090f3bc0d6b33a2ded
Alex LaFroscia <alex@lafroscia.com>
2016-02-20 11:09:31 -0500

    Update dependencies
    Dependencies Upgraded:
    Ember CLI -> 2.4.3
    Embe Suave -> 2.0.1
    
    Also, removed some unused Bower dependencies

---
+++ testem.js
@@ -0,0 +1,15 @@
+/* jshint node:true */
+/* jscs:disable */
+
+module.exports = {
+  framework: 'qunit',
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  launch_in_ci: [
+    'PhantomJS'
+  ],
+  launch_in_dev: [
+    'PhantomJS',
+    'Chrome'
+  ]
+};
