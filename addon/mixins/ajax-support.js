77046c958599dd8b827601661c5ed4a07d2358ba
Alex LaFroscia <alex@lafroscia.com>
2016-06-01 15:17:34 -0700

    Add mixin for ember-data integration

---
+++ addon/mixins/ajax-support.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+const {
+  Mixin,
+  inject: { service },
+  computed: { alias }
+} = Ember;
+
+export default Mixin.create({
+
+  ajaxService: service('ajax'),
+
+  host: alias('ajaxService.host'),
+
+  namespace: alias('ajaxService.host'),
+
+  headers: alias('ajaxService.headers'),
+
+  ajax(url, type, options) {
+    options = this.ajaxOptions(...arguments);
+    return this.get('ajaxService').request(url, options);
+  },
+
+  ajaxOptions(url, type, options = {}) {
+    options.type = type;
+    return this.get('ajaxService').options(url, options);
+  }
+});
