3011250340126a51067e423b4aae79fc49fa4594
Brandon Ellis <bellis@innovu.com>
2016-06-28 15:42:52 -0400

    replace integration test with unit tests

--- tests/integration/components/ajax-get-test.js
+++ tests/integration/components/ajax-get-test.js
@@ -100,5 +100,33 @@ describeComponent(
         expect(this.$('.ajax-get p').text()).to.equal('0');
       });
     });
+
+    it('clicking Load Data loads data', function() {
+      const PAYLOAD = [{ title: 'Foo' }, { title: 'Bar' }, { title: 'Baz' }];
+
+      this.server.get('/foo', json(200, PAYLOAD), 300);
+
+      this.render(hbs`
+        {{#ajax-get url="/foo" as |data isLoaded|}}
+          {{#if isLoaded}}
+            <ul>
+            {{#each data as |post|}}
+              <li>{{post.title}}</li>
+            {{/each}}
+            </ul>
+          {{else}}
+            <button {{action data}}>Load Data</button>
+          {{/if}}
+        {{/ajax-get}}
+      `);
+
+      this.$(`.ajax-get button`).click();
+
+      return wait().then(() => {
+        expect(this.$('.ajax-get li:eq(0)').text()).to.equal('Foo');
+        expect(this.$('.ajax-get li:eq(1)').text()).to.equal('Bar');
+        expect(this.$('.ajax-get li:eq(2)').text()).to.equal('Baz');
+      });
+    });
   }
 );
