724846dda633e5114fc13bfb48aabfc727651bbb
Derek Wickern <dwickern@gmail.com>
2016-08-05 09:45:58 -0700

    add failed test for duplicate namespace

--- tests/acceptance/ember-data-integration-test.js
+++ tests/acceptance/ember-data-integration-test.js
@@ -6,6 +6,7 @@ const { equal } = assert;
 import destroyApp from 'dummy/tests/helpers/destroy-app';
 import startApp from 'dummy/tests/helpers/start-app';
 
+import AjaxService from 'ember-ajax/services/ajax';
 import Pretender from 'pretender';
 import { jsonResponse } from 'dummy/tests/helpers/json';
 
@@ -22,6 +23,31 @@ describe('Acceptance | ember data integration', function() {
   });
 
   it('can apply the Ember Ajax mixin to an Ember Data adapter', function() {
+    server.get('api/posts/1', function() {
+      return jsonResponse(200, {
+        data: {
+          id: 1,
+          type: 'post',
+          attributes: {
+            title: 'Foo'
+          }
+        }
+      });
+    });
+
+    visit('/ember-data-test');
+
+    andThen(function() {
+      equal(currentURL(), '/ember-data-test');
+    });
+  });
+
+  it('can set the namespace for all ajax requests', function() {
+    application.register('service:ajaxWithNs', AjaxService.extend({
+      namespace: 'api'
+    }));
+    application.inject('adapter:application', 'ajaxService', 'service:ajaxWithNs');
+
     server.get('api/posts/1', function() {
       return jsonResponse(200, {
         data: {
