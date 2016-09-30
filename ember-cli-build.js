b32b9ee5ede69e09a162eb98d354f7de6297f679
Alex LaFroscia <alex@lafroscia.com>
2016-09-30 00:10:09 -0700

    Add code coverage (#164)
    * Install `ember-cli-code-coverage` addon
    
    * Configure the CI to run and report coverage
    
    * Add coverage badge to README

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,7 +13,5 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-  app.import(app.bowerDirectory + '/pretender/pretender.js');
-
   return app.toTree();
 };
