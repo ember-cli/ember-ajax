b208b7d5ddb9f9561db1234f21a5e1d9ee5ba3ee
joshuabremer <joshua.bremer@gmail.com>
2016-10-11 13:02:50 -0700

    Ember-ajax should return the errors payload of ConflictErrors.

--- addon/errors.js
+++ addon/errors.js
@@ -103,7 +103,7 @@ AbortError.prototype = Object.create(AjaxError.prototype);
  * @extends AjaxError
  */
 export function ConflictError() {
-  AjaxError.call(this, null, 'The ajax operation failed due to a conflict');
+  AjaxError.call(this, errors, 'The ajax operation failed due to a conflict');
 }
 
 ConflictError.prototype = Object.create(AjaxError.prototype);
