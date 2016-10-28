ce5379dcf60ed56a42b442ab2dfb5662298d8b19
Xaser Acheron <xaser.88@gmail.com>
2016-10-28 15:17:17 -0500

    changed get-header tests to use e'expect' rather than 'assert'

--- tests/unit/utils/get-header-test.js
+++ tests/unit/utils/get-header-test.js
@@ -1,20 +1,18 @@
 import getHeader from 'ember-ajax/utils/get-header';
 import { describe, it } from 'mocha';
-import { assert } from 'chai';
-
-const { equal } = assert;
+import { expect } from 'chai';
 
 describe('getHeader', function() {
   it('returns undefined when headers are undefined', function() {
     const header = getHeader(undefined);
 
-    equal(header, undefined, 'undefined is returned');
+    expect(header).to.equal(undefined, 'undefined is returned');
   });
 
   it('returns undefined when name is undefined', function() {
     const header = getHeader({}, undefined);
 
-    equal(header, undefined, 'undefined is returned');
+    expect(header).to.equal(undefined, 'undefined is returned');
   });
 
   it('matches result given by direct object access', function() {
@@ -24,9 +22,9 @@ describe('getHeader', function() {
       'date': 'Fri, 12 Feb 2016 19:21:00 GMT'
     };
 
-    equal(getHeader(headers, 'Content-Encoding'), headers['Content-Encoding'], 'matches direct object access');
-    equal(getHeader(headers, 'content-type'), headers['content-type'], 'matches direct object access');
-    equal(getHeader(headers, 'date'), headers.date, 'matches direct object access');
+    expect(getHeader(headers, 'Content-Encoding')).to.equal(headers['Content-Encoding'], 'matches direct object access');
+    expect(getHeader(headers, 'content-type')).to.equal(headers['content-type'], 'matches direct object access');
+    expect(getHeader(headers, 'date')).to.equal(headers.date, 'matches direct object access');
   });
 
   it('performs case-insensitive lookup', function() {
@@ -36,8 +34,8 @@ describe('getHeader', function() {
       'date': 'Fri, 12 Feb 2016 19:21:00 GMT'
     };
 
-    equal(getHeader(headers, 'CoNtEnT-EnCoDiNg'), headers['Content-Encoding'], 'matches case-insensitive header');
-    equal(getHeader(headers, 'Content-Type'), headers['content-type'], 'matches case-insensitive header');
-    equal(getHeader(headers, 'DATE'), headers.date, 'matches case-insensitive header');
+    expect(getHeader(headers, 'CoNtEnT-EnCoDiNg')).to.equal(headers['Content-Encoding'], 'matches case-insensitive header');
+    expect(getHeader(headers, 'Content-Type')).to.equal(headers['content-type'], 'matches case-insensitive header');
+    expect(getHeader(headers, 'DATE')).to.equal(headers.date, 'matches case-insensitive header');
   });
 });
