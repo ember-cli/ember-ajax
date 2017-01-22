df2f838bb75806be738fd7fa8b737588923ec174
James Herdman <james.herdman@gmail.com>
2017-01-22 17:08:32 -0500

    Upgrade Ember and Buddies to 2.10 (#228)

--- testem.js
+++ testem.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
 
 module.exports = {
-  framework: 'qunit',
-  test_page: 'tests/index.html?nocontainer',
-  disable_watching: true,
-  launch_in_ci: [
-    'Chrome'
+  "framework": "qunit",
+  "test_page": "tests/index.html?hidepassed",
+  "disable_watching": true,
+  "launch_in_ci": [
+    "Chrome"
   ],
-  launch_in_dev: [
-    'Chrome'
+  "launch_in_dev": [
+    "Chrome"
   ]
 };
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-const { Router: EmberRouter } = Ember;
+const {
+  Router: EmberRouter
+} = Ember;
 
 const Router = EmberRouter.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
