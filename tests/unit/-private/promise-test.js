fc0a47e06bd653582691ca0beb66cc129976829d
Alex LaFroscia <alex@lafroscia.com>
2017-02-06 10:57:27 -0800

    Add XHR property to promises (#223)
    * Add XHR property to promises
    
    * Fix request-abort test case
    
    * Use correct error catching method
    
    * Add tests for the AJAXPromise class specifically

---
+++ tests/unit/-private/promise-test.js
@@ -0,0 +1,30 @@
+import { describe, it, beforeEach } from 'mocha';
+import { expect } from 'chai';
+
+import AJAXPromise from 'ember-ajax/-private/promise';
+
+describe('Unit | AJAXPromise Class', function() {
+  it('propigates the `xhr` property after calling `.then`', function() {
+    const promise = AJAXPromise.resolve('foo');
+    promise.xhr = { foo: 'bar' };
+
+    const childPromise = promise.then((d) => d);
+    expect(childPromise.xhr).to.equal(promise.xhr);
+  });
+
+  it('propigates the `xhr` property after calling `.catch`', function() {
+    const promise = AJAXPromise.resolve('foo');
+    promise.xhr = { foo: 'bar' };
+
+    const childPromise = promise.catch((d) => d);
+    expect(childPromise.xhr).to.equal(promise.xhr);
+  });
+
+  it('propigates the `xhr` property after calling `.finally`', function() {
+    const promise = AJAXPromise.resolve('foo');
+    promise.xhr = { foo: 'bar' };
+
+    const childPromise = promise.finally((d) => d);
+    expect(childPromise.xhr).to.equal(promise.xhr);
+  });
+});
