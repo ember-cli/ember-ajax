59a60e177878d5e20a12e89a10df272389717471
Alex LaFroscia <alex@lafroscia.com>
2017-11-26 20:30:33 -0500

    Replace `ember-suave` with the Ember ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,52 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:ember-suave/recommended'
-  ],
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
+  plugins: ['ember'],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended'
+  ],
   env: {
     'browser': true
   },
   rules: {
     // Built-in Rule Config
-    'prefer-const': 'error',
+    'prefer-const': 'error'
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        './index.js',
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    },
 
-    // Ember Suave Rule Config
-    'ember-suave/no-const-outside-module-scope': 'off'
-  }
+    // test files
+    {
+      files: ['tests/**/*.js'],
+      excludedFiles: ['tests/dummy/**/*.js'],
+      env: {
+        embertest: true
+      },
+      rules: {
+        // Useful for testing; not worried about leaking state
+        'ember/avoid-leaking-state-in-ember-objects': 'off'
+      }
+    }
+  ]
 };
