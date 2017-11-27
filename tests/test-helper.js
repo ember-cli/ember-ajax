d5bbb7ef489ec9d876a9af463011d305943cefeb
Alex LaFroscia <alex@lafroscia.com>
2017-11-26 20:30:33 -0500

    Replace `ember-suave` with the Ember ESLint config

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,9 +1,9 @@
-import $ from 'jquery';
+import jq from 'jquery';
 import resolver from './helpers/resolver';
 import { setResolver } from 'ember-mocha';
 
 setResolver(resolver);
 
 if (window.location.search.indexOf('nocontainer') > -1) {
-  $('#ember-testing-container').css({ visibility: 'hidden' });
+  jq('#ember-testing-container').css({ visibility: 'hidden' });
 }
