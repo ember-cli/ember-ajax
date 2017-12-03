f52a46c68e0c28471e79cb1d5cc22dbbd04a6b0a
Alex LaFroscia <alex@lafroscia.com>
2017-12-02 21:36:28 -0800

    Minimal transpilation during tests

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,4 +1,9 @@
 /* eslint-env node */
+
+const isTesting = process.env['NODE_ENV'] === 'test';
+
 module.exports = {
-  browsers: ['ie 9', 'last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions']
+  browsers: isTesting
+    ? ['last 1 Chrome versions']
+    : ['ie 9', 'last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions']
 };
